d0c70e45ab8a67d5da5c76431b09f60c3a9c4f34
Raghavendra-Devale <raghavendradevale07@gmail.com>
2026-06-22 11:40:41 +0530

    feat: implement push notification system with Firebase integration and device token management

---
+++ Botiq-Backend/src/main/java/com/dfive/botiq/controllers/PushNotificationController.java
@@ -0,0 +1,4 @@
+package com.dfive.botiq.controllers;
+
+public class PushNotificationController {
+}
---
+++ Botiq-Backend/src/main/java/com/dfive/botiq/services/PushNotificationService.java
@@ -0,0 +1,35 @@
+package com.dfive.botiq.service;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PushNotificationService {
+
+    public String sendNotification(
+            String token,
+            String title,
+            String body) throws Exception {
+
+        FirebaseApp notificationApp =
+                FirebaseApp.getInstance("NOTIFICATION_APP");
+
+        Message message =
+                Message.builder()
+                        .setToken(token)
+                        .setNotification(
+                                Notification.builder()
+                                        .setTitle(title)
+                                        .setBody(body)
+                                        .build()
+                        )
+                        .build();
+
+        return FirebaseMessaging
+                .getInstance(notificationApp)
+                .send(message);
+    }
+}
